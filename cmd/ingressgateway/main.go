cda9cd86dea74e3fda2c595a0040bac42392b884
Hiroya Onoe <git@onoe.dev>
2023-11-24 18:37:46 +0900

    CICD

--- cmd/ingressgateway/main.go
+++ cmd/ingressgateway/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, ingressgateway")
+	port := flag.String("port", "8080", "port to serve on")
+	envID := flag.String("env-id", "", "environment to route connections")
+	defaultAddr := flag.String("default-addr", "", "default address")
+
+	flag.Parse()
+
+	fmt.Println("port:", *port)
+	fmt.Println("env-id:", *envID)
+	fmt.Println("default-addr:", *defaultAddr)
 }
